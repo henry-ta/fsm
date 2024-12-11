29ed2d63da3980919b2396d20f9ed48bf65d59fe
Henry Ta <henry.ta@tuta.io>
2024-12-10 22:25:13 -0800

    feat(cpp): create `AState` cl
    `AState` abstract class is a blueprint for other states. It provides
    some common implementation for its subclasses.

--- cpp/Makefile
+++ cpp/Makefile
@@ -1,15 +1,28 @@
-OBJS = main.o state_manager.o
+OBJS = main.o state_manager.o state.o
 CC = g++
 
+#---------------Executable---------------#
 FSM: $(OBJS)
 	g++ -o FSM $(OBJS)
 	rm -f $(OBJS)	# Remove *.o files
 
+#---------------Object Files---------------#
 main.o:	
 	$(CC) -c ./main.cpp
 
-state_manager.o:	
+state_manager.o:
 	$(CC) -c ./src/state_manager.cpp
 
+state.o:
+	$(CC) -c ./src/state.cpp
+
+
+#---------------Commands---------------#
 clean:	
 	rm -f core FSM $(OBJS)
+
+run:
+	./FSM
+
+rebuild:
+	make clean; make
---
+++ cpp/inc/state.h
@@ -0,0 +1,31 @@
+#ifndef STATE_H
+#define STATE_H
+
+// Abstract (a blueprint with some implementations, cannot be instantiated)  
+class AState {      
+    private:
+        bool isFinished;
+        int nextStage;
+        int userAction;
+        bool isSelected;
+
+    public:
+        AState();
+
+        void setIsFinished(bool b);
+        int getIsFinished();
+
+        void setNextStage(int stageID);
+        int getNextStage();
+
+        void setUserAction(int stageID);
+        int getUserAction();
+
+        void setIsSelected(bool b);
+        bool getIsSelected();
+
+        virtual void update() = 0;  // pure virtual func (abstract method)
+        virtual void display() = 0; // pure virtual func (abstract method)
+};  // * Don't forget `;`
+
+#endif  // STATE_H
